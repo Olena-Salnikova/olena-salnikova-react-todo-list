0d3132dae8f9338c454c3916c7650ad3a44875cc
Olena Salnikova <nameless.hire5@gmail.com>
2026-04-14 17:59:51 -0400

    Week 02: Add TodoForm and TodoList components for managing todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css'
+import TodoList from './TodoList.jsx';
+import TodoForm from './TodoForm.jsx';
 
 function App() {
-// Create an array of todoList
-const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-]
-// Render the todoList in an unordered list  
   return (
     <div>
       <h1>My Todos</h1>
-      <ul>
-        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
+      <TodoForm />
+      <TodoList />
     </div>
   )
 }
 
-export default App
+export default App;
