42173d373c743bbbedcc04f6c242bf412eea8f1b
Olena Salnikova <nameless.hire5@gmail.com>
2026-05-19 23:24:24 -0400

    Lesson 07 - Data fetching, UI update strategies

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-import './App.css'
+import './App.css';
+import Header from './shared/Header.jsx';
+import TodosPage from './features/Todos/TodosPage.jsx';
+import Logon from './features/Logon.jsx';
 import { useState } from 'react';
-import TodoForm from './features/TodoForm.jsx';
-import TodoList from './features/TodoList/TodoList.jsx';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-
-  // Adds a new todo to the start of the list
-  function addTodo(todoTitle) {
-    const newTodo = {
-      id: Date.now(),
-      title: todoTitle,
-      isCompleted: false
-    };
-    setTodoList(previous => [newTodo, ...previous]);
-  }
-
-  function updateTodo(editedTodo) {
-    const updatedTodos = todoList.map(todo =>
-      todo.id === editedTodo.id ? { ...editedTodo } : todo
-    );
-    setTodoList(updatedTodos);
-  }
-
-  function completeTodo(id) {
-    setTodoList(prevList =>
-      prevList.map(todo =>
-        todo.id === id
-          ? { ...todo, isCompleted: true }
-          : todo
-      )
-    );
-  }
+  const [_email, setEmail] = useState('');
+  const [token, setToken] = useState('');
 
   return (
-    <div>
-      <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo} />
-      <TodoList 
-        todoList={todoList} 
-        onUpdateTodo={updateTodo} 
-        onCompleteTodo={completeTodo} 
-      />
-    </div>  
-  )
+    <>
+      <Header token={token} onSetToken={setToken} onSetEmail={setEmail} />
+      {token
+        ? <TodosPage token={token} />
+        : <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      }
+    </>
+  );
 }
 
 export default App;
